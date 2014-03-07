47499348419369c3cdf2d6e751b480dde8b86e7d
unknown <strnisaj@R311-14.std.fmf.uni-lj.si>
2014-03-07 11:41:20 +0100

    Dodal sem prvo verzijo sudoku-ja (prevajanje na SAT)

---
+++ Izjave.py
@@ -0,0 +1,142 @@
+class Var():
+    def __init__(self,ime,vrednost):
+        self.ime = ime
+        self.vrednost = vrednost
+
+    def __repr__(self):
+        return self.ime
+
+    def vrni(self):
+        return self
+
+    def izracun(self):
+        return self.vrednost
+
+    def poenostavi(self):
+        return self
+
+class Tru(Var):
+    def __init__(self):
+        Var.__init__(self,'T',True)
+
+class Fal(Var):
+    def __init__(self):
+        Var.__init__(self,'F',False)
+
+class And():
+    def __init__(self, izjave):
+        self.izjave = izjave
+        ime = '('
+        for i in self.izjave:
+            if self.izjave.index(i)== 0:
+                ime = ime + i.ime
+            else:
+               ime = ime + ' and {0}'.format(i.ime) 
+        self.ime = ime + ')'  
+
+    def __repr__(self):
+        return self.ime
+
+    def vrni(self):
+        return self.izjave
+
+    def izracun(self):
+        rez = True
+        for i in self.izjave:
+            rez = rez and i.izracun()
+        return rez
+
+    def poenostavi(self):
+        for i in self.izjave:
+            if type(i) is Tru:
+                self.izjave.remove(i)
+            elif type(i) is Fal:
+                return Fal()
+        if len(self.izjave) == 1:
+            return self.izjave[0].poenostavi()
+        return And([i.poenostavi() for i in self.izjave])
+    
+
+class Or():
+    def __init__(self, izjave):
+        self.izjave = izjave
+        ime = '('
+        for i in self.izjave:
+            if self.izjave.index(i)== 0:
+                ime = ime + i.ime
+            else:
+               ime = ime + ' or {0}'.format(i.ime) 
+        self.ime = ime + ')'
+
+    def __repr__(self):
+        return self.ime
+
+    def vrni(self):
+        return self.izjave
+
+    def izracun(self):
+        rez = True
+        for i in self.izjave:
+            rez = rez or i.izracun()
+        return rez
+
+    def poenostavi(self):
+        for i in self.izjave:
+            if type(i) is Fal:
+                self.izjave.remove(i)
+            elif type(i) is Tru:
+                return Tru()
+        if len(self.izjave) == 1:
+            return self.izjave[0].poenostavi()
+        return Or([i.poenostavi() for i in self.izjave])
+
+class Not():
+    def __init__(self,A):
+        self.A = A
+        self.ime = 'not{0}'.format(self.A.ime)
+
+    def __repr__(self):
+        return self.ime
+
+    def vrni(self):
+        return self.A
+
+    def izracun(self):
+        return not self.A.izracun()
+
+    def poenostavi(self):
+        if type(self.A) is Var:
+            return self
+        elif type(self.A) is And:
+            return Or([Not(i).poenostavi() for i in self.A.vrni()])
+        elif type(self.A) is Or:
+            return And([Not(i).poenostavi() for i in self.A.vrni()])
+        elif type(self.A) is Not:
+            return self.A.vrni().poenostavi()
+
+
+
+def test():
+    a = Var('A',True)
+    b = Var('B',False)
+    c = Var('C',False)
+    f = Fal()
+    t = Tru()
+    print(izjava)
+    print(izjava.izracun())
+    izjavA = izjava.poenostavi()
+    print(izjavA)
+    print(izjavA.izracun())
+    print(And(a,t))
+    print(And(a,t).poenostavi())
+    print(Or(b,f))
+    print(Or(b,f).poenostavi())
+   
+
+def bla(*args):
+    a = ''
+    for i in args:
+        a = a+i
+    print(a)
+
+    
---
+++ Sudoku.py
@@ -0,0 +1,104 @@
+import Izjave
+# funkcija sprejme dva seznama. Seznam sez predstavlja nerešen sudoku z nekaterimi že vpisanimi številkami,
+# seznam sezR pa rešen sudoku, ki ga predstavlja sez. Funkcija vrne izjavo, ki je resnièna, èe je sezR res rešen
+# sez in neresnièna, èe to ni res.
+def sudoku(sez,sezR):
+    n = len(sezR)
+    m = int(n**0.5)
+    # Sestavimo seznam spremenljivk spremen. Spremenljivka X(i,j,k) je na mestu spremen[i][j][k-1] in po meni:
+    # "V polju (i,j) je zapisano število k"
+    # Sestavimo tudi seznam spremenljivk spremenK. V tem seznamu element spremenK[i][j] predstavlja kvadratek (i,j)
+    # pri èemer gresta i,j od 0 do m-1, kjer je m = sqrt(n). V vsakem elementu spremenK[i][j] je n seznamov( toliko
+    # kolikor je možnih števil. V k+1-em so spremenljivke X(i,j,k) za vse i,j med 0 in m-1.
+    spremen = []
+    spremenK = []
+    for i in range(n):
+        spremen.append([])
+        if i<m:
+            spremenK.append([])
+        for j in range(n):
+            spremen[i].append([])
+            if j<m and i<m:
+                spremenK[i].append([])
+            for k in range(1,n+1):
+                x = Izjave.Var('X({0},{1},{2})'.format(i,j,k),sezR[i][j]==k)
+                spremen[i][j].append(x)
+                if len(spremenK[i//m][j//m]) < n:
+                    spremenK[i//m][j//m].append([])
+                spremenK[i//m][j//m][k-1].append(x)                      
+    # Sestavimo izjavo: "V vsakem polju (i,j) je zapisano vsaj eno število k od 1 do n."
+    seznam1 = []
+    for i in range(n):
+        for j in range(n):
+            seznam1.append(Izjave.Or(spremen[i][j]))
+    izjava1 = Izjave.And(seznam1)
+
+    # Sestavimo izjavo: "V nobenem polju (i,j) nista zapisani dve števili."
+    seznam2 = []
+    for i in range(n):
+        for j in range(n):
+            seznam21 = []
+            for k in range(1,n+1):
+                for l in range(k+1,n+1):
+                    seznam21.append(Izjave.Not(Izjave.And([spremen[i][j][k-1],spremen[i][j][l-1]])))
+            seznam2.append(Izjave.And(seznam21))
+    izjava2 = Izjave.And(seznam2)
+    
+    # Sestavimo izjavo: "rešen in nerešen sudoku imata se strinjata na mestih, kjer so
+    #                     v nerešenm sudokuju že vpisana števila"  
+    
+    seznam3 = []
+    for i in range(n):
+        for j in range(n):
+            if sez[i][j] != 0:
+                seznam3.append(spremen[i][j][sez[i][j]-1])
+    izjava3 = Izjave.And(seznam3)
+
+    # Sestavimo izjavo: "V vsaki vrstici so sama razlièna števila"
+    seznam4 = []
+    for i in range(n):
+        for j in range(n):
+            for k in range(1,n+1):
+                for l in range(j+1,n):
+                    seznam4.append(Izjave.Not(Izjave.And([spremen[i][j][k-1],spremen[i][l][k-1]])))
+    izjava4 = Izjave.And(seznam4)
+
+    # Sestavimo izjavo: "V vsakem stolpcu so sama razlièna števila"
+    seznam5 = []
+    for j in range(n):
+        for i in range(n):
+            for k in range(1,n+1):
+                for l in range(i+1,n):
+                    seznam5.append(Izjave.Not(Izjave.And([spremen[i][j][k-1],spremen[l][j][k-1]])))
+    izjava5 = Izjave.And(seznam5)
+
+    # Sestavimo izjavo: "V vsakem m*m kvadratku se vsako število pojavi natanko enkrat"
+    seznam6 = []
+    for i in range(m):
+        for j in range(m):
+            for k in range(1,n+1):
+                for s in range(n):
+                    for r in range(s+1,n): 
+                        seznam6.append(Izjave.Not(Izjave.And([spremenK[i][j][k-1][s],spremenK[i][j][k-1][r]])))
+    izjava6 = Izjave.And(seznam6)
+
+    return Izjave.And([izjava1,izjava2,izjava3,izjava4,izjava5,izjava6])
+
+
+
+
+def test():
+   a = sudoku([[1,2,0,0],[3,0,1,0],[0,1,0,3],[0,0,2,1]],[[1,2,3,4],[3,4,1,2],[2,1,4,3],[4,3,2,1]])
+   print(a)
+   print(a.izracun())
+   b = sudoku([[1,2,0,0],[3,0,1,0],[0,1,0,3],[0,0,2,1]],[[1,2,2,4],[3,4,1,2],[2,1,4,3],[4,3,2,1]])
+   print(b)
+   print(b.izracun())
+
+
+
+
+
+
+
+    
